bf751fa8a0f4f3bd08fa5c4ebdfac51647c7893a
“EngEman1999” <eman170249@gmail.com>
2023-11-14 01:00:31 +0200

    Web structure

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,45 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/About', function () {
+  return 'welcome , This is my first project in the laravel';
+});
+Route::get('/Contact us', function (){
+  return 'You can contact us by phone : 011111111111';
+});
+Route::prefix('Support')->group(function () {
+  Route::get('/', function(){
+    return 'You can support us by : (Chat , Call , Ticket)';
+  });
+  Route::get('Chat', function(){
+    return 'Chat';
+  });
+  Route::get('Call', function(){
+    return 'Call';
+  });
+  Route::get('Ticket', function(){
+    return 'Ticket';
+  });
+});
+Route::prefix('Training')->group(function () {
+  Route::get('/', function(){
+    return 'Types of training we have : (HR , ICT , Marketing , Logistics)';
+  });
+  Route::get('HR', function(){
+    return 'HR';
+  });
+  Route::get('ICT', function(){
+    return 'ICT';
+  });
+  Route::get('Marketing', function(){
+    return 'Marketing';
+  });
+  Route::get('Logistics', function(){
+    return 'Logistics';
+  });
+});
+
+
 /*
 Route::get('/user/{name}/{age?}', function ($name , $age=0) {
     $m = 'The username is: ' . $name;
@@ -53,41 +92,4 @@ Route::prefix('product')->group(function () {
         
     });
 */
-Route::get('/About', function () {
-  return 'welcome , This is my first project in the laravel';
-});
-Route::get('/Contact us', function (){
-  return 'You can contact us by phone : 011111111111';
-});
-Route::prefix('Support')->group(function () {
-  Route::get('/', function(){
-    return 'You can support us by : (Chat , Call , Ticket)';
-  });
-  Route::get('Chat', function(){
-    return 'Chat';
-  });
-  Route::get('Call', function(){
-    return 'Call';
-  });
-  Route::get('Ticket', function(){
-    return 'Ticket';
-  });
-});
-Route::prefix('Training')->group(function () {
-  Route::get('/', function(){
-    return 'Types of training we have : (HR , ICT , Marketing , Logistics)';
-  });
-  Route::get('HR', function(){
-    return 'HR';
-  });
-  Route::get('ICT', function(){
-    return 'ICT';
-  });
-  Route::get('Marketing', function(){
-    return 'Marketing';
-  });
-  Route::get('Logistics', function(){
-    return 'Logistics';
-  });
-});
 
